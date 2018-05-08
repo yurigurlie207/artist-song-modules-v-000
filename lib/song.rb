6fbaf6f506977203ad66b9ac1aa9d3f9347f2cf7
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 18:02:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,9 @@ class Song
   #   @@songs.clear
   # end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   # def self.count
   #   self.all.count
