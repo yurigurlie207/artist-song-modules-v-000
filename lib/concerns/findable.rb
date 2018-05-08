d50784508392ec075ba097ebedd958f4db11736c
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 17:56:07 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  def ind_by_name(name)
+    all.detect{|a| a.name == name}
   end
 
   def self.find_by_name(name)
