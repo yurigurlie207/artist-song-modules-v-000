f081b7b51f9a3d4f294c54c6a5ce219fb6198d8a
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 18:14:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
