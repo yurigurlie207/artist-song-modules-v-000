b0003677ecc48bac6660e5786bad40ad7ac7a3f2
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 18:15:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   extend Findable
-  include Paramable::InstanceMethods
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
