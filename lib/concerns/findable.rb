2f42efe692682ecd2e8491e796da50e6317541e9
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 17:56:23 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,7 @@
 module Findable
 
-  def ind_by_name(name)
+  def find_by_name(name)
     all.detect{|a| a.name == name}
-  end
-
-  def self.find_by_name(name)
-    all.detect{|a| a.name == name}
-  end
-
+end
 
 end
